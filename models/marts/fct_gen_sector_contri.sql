5ff9bb9bf8786f91e1b65c04e8accbc4c61cbae1
Lil Baby <112095953+LilsBaby@users.noreply.github.com>
2026-02-08 20:38:47 +0700

    Renamed energy generation sector

--- models/marts/fct_gen_sector_contri.sql
+++ models/marts/fct_gen_sector_contri.sql
@@ -5,8 +5,8 @@ WITH generation AS (
 )
 
 SELECT 
-    g.energy_type,
+    replace(g.energy_type, 'Toutes les filières', 'All sectors') AS energy_type,
     round(sum(g.twh), 1) AS twh
 FROM generation g
 GROUP BY 1
-ORDER BY 2
+ORDER BY 2 DESC
