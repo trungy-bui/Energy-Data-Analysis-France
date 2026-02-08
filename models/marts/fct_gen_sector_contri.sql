80d0b50ddb6c00202e4c63b251f3bef77c868b55
Lil Baby <112095953+LilsBaby@users.noreply.github.com>
2026-02-08 20:44:17 +0700

    Renamed and ordered the generation TWH

--- models/marts/fct_gen_sector_contri.sql
+++ models/marts/fct_gen_sector_contri.sql
@@ -5,8 +5,8 @@ WITH generation AS (
 )
 
 SELECT 
-    replace(g.energy_type, 'Toutes les filières', 'All sectors') AS energy_type,
-    round(sum(g.twh), 1) AS twh
+    REPLACE(g.energy_type, 'Toutes les filières', 'All sectors') AS energy_type,
+    ROUND(sum(g.twh), 1) AS twh
 FROM generation g
 GROUP BY 1
 ORDER BY 2 DESC
