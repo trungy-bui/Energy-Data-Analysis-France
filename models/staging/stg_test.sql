9086662c50741e2b56ed7edacc1d4d5296530b0c
Jeremy Bui <112095953+trungy-bui@users.noreply.github.com>
2026-07-25 16:21:03 +0700

    Update stg_test.sql

--- models/staging/stg_test.sql
+++ models/staging/stg_test.sql
@@ -3,7 +3,7 @@ WITH source_capacity AS (
     FROM {{ source('CAPACITY_RAW', 'CAPACITY_HISTORY') }}
 )
 
-SELECT * FROM source_capacity
+SELECT * FROM source_capacity;;
 
 WITH source_something AS (
     SELECT *
