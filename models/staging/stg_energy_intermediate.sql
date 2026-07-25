4318b992cfcbe853ee99337d9168f021a37c2e4a
Jeremy Bui <112095953+trungy-bui@users.noreply.github.com>
2026-07-25 16:15:49 +0700

    Add SELECT statement for source_generation data
    Added a new SELECT statement to retrieve data from source_generation.

--- models/staging/stg_energy_intermediate.sql
+++ models/staging/stg_energy_intermediate.sql
@@ -3,4 +3,6 @@ WITH source_generation AS (
     FROM {{ source('GENERATION_RAW', 'GENERATION_HISTORY') }}
 ),
 
+SELECT * FROM source_generation
+
 SELECT SELECT(*), testing_generaiton FROM source_generation
